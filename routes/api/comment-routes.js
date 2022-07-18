05d8eef90e9f09cf98c5814de1268a4abcf6fe05
Matthew Wiser <matt.wiser@icloud.com>
2022-07-17 23:17:41 -0500

    implemented Comment model and api routes

---
+++ routes/api/comment-routes.js
@@ -0,0 +1,89 @@
+const router = require('express').Router();
+const { Comment, User, Post } = require('../../models');
+
+//get all comments
+router.get('/', (req, res) => {
+    Comment.findAll({
+        include: [
+            {
+                model: User,
+                attributes: ["username"],
+            },
+            {
+                model: Post,
+                attributes: ["title"],
+            }
+        ]
+    })
+    .then(commentData => {
+        res.json({commentData})
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    });
+});
+
+//create a comment
+//expects {comment_text: "", user_id: "", post_id: ""}
+router.post('/', ({body}, res) => {
+    Comment.create({
+        comment_text: body.comment_text,
+        user_id: body.user_id,
+        post_id: body.post_id
+    })
+    .then(commentData => {
+        res.json({commentData})
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    });
+});
+//update a comment
+//expects {comment_text: ""}
+router.put('/:id', ({body, params}, res) => {
+    Comment.update(
+        {
+            comment_text: body.comment_text
+        },
+        {
+            where: {
+                id: params.id
+            }
+        }
+    )
+    .then(commentData => {
+        if (!commentData) {
+            res.status(404).json({message: "No comment found with that id!"});
+            return;
+        }
+        res.json({commentData});
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    });
+});
+
+//delete a comment
+router.delete('/:id', ({params}, res) => {
+    Comment.destroy({
+        where: {
+            id: params.id
+        }
+    })
+    .then(commentData => {
+        if (!commentData) {
+            res.status(404).json({message: "No comment found with that id!"});
+            return;
+        }
+        res.json({commentData});
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    });
+});
+
+module.exports = router;
--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,16 +1,24 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Comment } = require('../../models');
 
 //find all posts
 router.get("/", (req, res)=> {
     console.log("=============================");
     Post.findAll({
         attributes: ["id", "content", "title", "created_at"],
+        order:[["created_at", "DESC"]],
         include:[
+            {
+                model: Comment,
+                attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+                include: {
+                    model: User,
+                    attributes: ["username"]
+                }
+            },
             {
                 model: User,
                 attributes: ["username"],
-                order:[["created_at", "DESC"]]
             }
         ]
     })
@@ -31,6 +39,14 @@ router.get("/:id", ({params}, res) => {
         },
         attributes: ["id", "content", "title", "created_at"],
         include:[
+            {
+                model: Comment,
+                attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+                include: {
+                    model: User,
+                    attributes: ["username"]
+                }
+            },
             {
                 model: User,
                 attributes: ["Username"]
