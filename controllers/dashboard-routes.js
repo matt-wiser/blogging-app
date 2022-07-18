8edc8c7164537e9bc5eef2022870c5df0e5e3b0a
Matthew Wiser <matt.wiser@icloud.com>
2022-07-18 16:07:32 -0500

    added bootstrap styling

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -26,7 +26,7 @@ router.get('/', withAuth, (req, res) => {
     })
     .then(postData => {
         const posts = postData.map(post => post.get({plain: true}))
-        res.render('dashboard', { posts, loggedIn: true });
+        res.render('dashboard', { posts, loggedIn: true});
     })
     .catch(err => {
         console.log(err);
--- utils/helpers.js
+++ utils/helpers.js
@@ -11,4 +11,7 @@ module.exports = {
     
         return word;
       },
+      increment: (index) => {
+        return parseInt(index + 1)
+      }
   }
